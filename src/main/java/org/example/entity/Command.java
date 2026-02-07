f4b1a96866a589274738e1e90911a609fd959e6f
Kirtianurag <kirtianurag2003@gmail.com>
2026-02-07 10:35:16 +0530

    Initial commit - InMemoryDBProject

---
+++ src/main/java/org/example/entity/Command.java
@@ -0,0 +1,8 @@
+package org.example.entity;
+
+public class Command {
+    public CommandType type;
+    public Integer key;
+    public String rawValue;
+    public long ttl = -1;
+}
---
+++ src/main/java/org/example/entity/CommandType.java
@@ -0,0 +1,5 @@
+package org.example.entity;
+
+public enum CommandType {
+    PUT , GET , DELETE , START , STOP , EXIT , SHOW
+}
